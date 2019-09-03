a40a40100e6ae12e0209c0a32857ad98a5ae2866
jerremeirago <jrago@melistechnology.com>
2019-09-03 20:17:47 +0800

    updated translations

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -5,4 +5,10 @@ return [
     'tr_melis_code_example_lumen_tool_title' => 'Lumen Tool Demo',
     'tr_melis_code_example_lumen_tool_head_title' => 'A tool embryo made in Lumen',
     'tr_melis_code_example_lumen_tool_head_desc' => 'This demo tool has for goal to show how you can use the framework Lumen inside Melis Platform, so you\'re not locked on ZF.',
+    'tr_melis_lumen_demo_tool_sample_1_heading' => 'The table below shows the data fetched from the database using Lumen.',
+    'tr_melis_lumen_demo_tool_sample_2_heading' => 'The table below shows the data fetched from Melis Platform Services using Lumen.',
+    'tr_melis_lumen_table1_heading_name' => 'Name',
+    'tr_melis_lumen_table1_heading_songs' => 'Songs',
+
+
 ];
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -5,4 +5,8 @@ return [
     'tr_melis_code_example_lumen_tool_title' => 'Outil Demo en Lumen',
     'tr_melis_code_example_lumen_tool_head_title' => 'Un embryon d\'outil fait en Lumen',
     'tr_melis_code_example_lumen_tool_head_desc' => 'Ce outil de demo a pour but de montrer comment utiliser le framework Lumen à l\'intérieur de Melis Platform, et ne pas être bloqué en ZF.',
+    'tr_melis_lumen_demo_tool_sample_1_heading' => 'Le tableau ci-dessous affiche les données extraites de la base de données en utilisant Lumen.',
+    'tr_melis_lumen_demo_tool_sample_2_heading' => 'Le tableau ci-dessous affiche les données extraites des services de Melis Platform en utilisant Lumen.',
+    'tr_melis_lumen_table1_heading_name' => 'Nom',
+    'tr_melis_lumen_table1_heading_songs' => 'Chansons',
 ];
