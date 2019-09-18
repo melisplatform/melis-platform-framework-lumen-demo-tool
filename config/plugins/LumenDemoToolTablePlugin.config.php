4bee9de189ce7b8b625fcacc4541fabe753615c7
jerremeirago <jrago@melistechnology.com>
2019-09-18 19:02:27 +0800

    added subection

--- config/plugins/LumenDemoToolTablePlugin.config.php
+++ config/plugins/LumenDemoToolTablePlugin.config.php
@@ -16,7 +16,10 @@ return array(
                         'template_path' => array('MelisLumenDemotool/render-plugin'),
                     ),
                     'melis' => array(
-                        'section' => 'MelisCore',
+                        'subcategory' => array(
+                            'id' => 'thirdPartyFrameworks',
+                            'title' => 'Frameworks'
+                        ),
                         'name' => 'Lumen Demo Tool Table Plugin',
                         'thumbnail' => '/MelisPlatformFrameworkLumenDemoTool/plugins/images/LumenDemoToolDisplayTable_thumb.jpg',
                         'description' => 'Lumen demo tool display table plugin',
@@ -28,89 +31,6 @@ return array(
                             'js' => [],
                         ],
                         'js_initialization' => [],
-                        /*'modal_form' => [
-                            // <editor-fold desc="Modal Settings">
-                            'melis_framework_lumen_demo_tool_list_plugin_template_form' => [
-                                'tab_title' => 'tr_front_plugin_tab_properties',
-                                'tab_icon'  => 'fa fa-cog',
-                                'tab_form_layout' => 'MelisCmsNews/plugin/modal/modal-template-form',
-                                'attributes' => [
-                                    'name' => 'melis_framework_lumen_demo_tool_list_plugin_template_form',
-                                    'class' => 'melis_framework_lumen_demo_tool_list_plugin_template_form',
-                                    'method' => '',
-                                    'action' => '',
-                                ],
-                                'elements' => [
-                                    [
-                                        'spec' => [
-                                            'name' => 'template_path',
-                                            'type' => 'MelisEnginePluginTemplateSelect',
-                                            'options' => [
-                                                'label' => 'tr_melis_Plugins_Template',
-                                                'tooltip' => 'tr_melis_Plugins_Template tooltip',
-                                                'empty_option' => 'tr_melis_Plugins_Choose',
-                                                'disable_inarray_validator' => true,
-                                            ],
-                                            'attributes' => [
-                                                'id' => 'id_page_tpl_id',
-                                                'class' => 'form-control',
-                                                'required' => 'required',
-                                            ]
-                                        ]
-                                    ],
-                                    [
-                                        'spec' => [
-                                            'name' => 'lumen_route',
-                                            'type' => 'text',
-                                            'options' => [
-                                                'label' => 'Lumen route',
-                                                'tooltip' => 'Url that is based from Lumen',
-                                                'empty_option' => 'tr_melis_Plugins_Choose',
-                                                'disable_inarray_validator' => true,
-                                            ],
-                                            'attributes' => [
-                                                'id' => 'lumen_route',
-                                                'class' => 'form-control',
-                                                'required' => 'required'
-                                            ]
-                                        ]
-                                    ],
-                                    'input_filter' => [
-                                        'template_path' => [
-                                            'name' => 'template_path',
-                                            'required' => true,
-                                            'validators' => [
-                                                [
-                                                    'name' => 'NotEmpty',
-                                                    'options' => [
-                                                        'messages' => [
-                                                            \Zend\Validator\NotEmpty::IS_EMPTY => 'tr_front_template_path_empty',
-                                                        ],
-                                                    ],
-                                                ],
-                                            ],
-                                            'filters' => [],
-                                        ],
-                                        'lumen_route' => [
-                                            'name' => 'lumen_route',
-                                            'required' => true,
-                                            'validators' => [
-                                                [
-                                                    'name' => 'String',
-                                                    'options' => [
-                                                        'messages' => [
-                                                            \Zend\Validator\Digits::STRING_EMPTY => 'tr_front_common_input_empty',
-                                                        ],
-                                                    ],
-                                                ],
-                                            ],
-                                            'filters' => [],
-                                        ],
-                                    ]
-                                ]
-                            ]
-                            // </editor-fold>
-                        ]*/
                     ),
                 ),
             ),
