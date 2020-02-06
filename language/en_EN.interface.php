9597de09246e4c07fec43e907753f1f9daf630b9
Junry Zabate <jzabate@melistechnology.com>
2020-02-06 16:59:04 +0800

    edit for jquery 3 and add bootstrap 4 classes

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -2,4 +2,11 @@
 
 return [
     'tr_melis_code_example_lumen_tool_title' => 'Lumen Tool Demo',
+     /*
+    * Plugin translations
+    */
+    'tr_melis_platform_framework_lumen_demo_tool_plugin_name' => 'Lumen Demo Plugin',
+    'tr_melis_platform_framework_lumen_demo_tool_plugin_description' => 'This plugin displays content requested from Lumen.',
+    'tr_PluginSection_melisplatformframeworklumendemotool' => 'Melis Platform Framework Lumen Demo Tool',
+    'tr_modal_plugin_no_parameters' => 'No parameters to edit.',
 ];
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -2,4 +2,11 @@
 
 return [
     'tr_melis_code_example_lumen_tool_title' => 'Outil Demo en Lumen',
+    /*
+     * Plugin translations
+     */
+    'tr_melis_platform_framework_lumen_demo_tool_plugin_name' => 'Lumen Demo Plugin',
+    'tr_melis_platform_framework_lumen_demo_tool_plugin_description' => 'Ce plugin affiche du contenu tiré de Lumen.',
+    'tr_PluginSection_melisplatformframeworklumendemotool' => 'Melis Platform Framework Lumen Demo Tool',
+    'tr_modal_plugin_no_parameters' => 'Aucun paramètre à éditer.',
 ];
